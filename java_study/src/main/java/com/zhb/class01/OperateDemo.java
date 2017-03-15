1a24386bdc11924d66bfac4007b300fc1085d3fa
zhb <542221222@qq.com>
2017-03-15 23:46:31 +0800

    null

---
+++ java_study/src/main/java/com/zhb/class01/OperateDemo.java
@@ -0,0 +1,13 @@
+package com.zhb.class01;
+
+/**
+ * Created by zhb on 17-3-15.
+ */
+public class OperateDemo {
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        y=x++;
+        System.out.println(y);
+    }
+}
